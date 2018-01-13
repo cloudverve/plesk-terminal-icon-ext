5ddb5bd87a24b8a479fd3557f2479524819b3c2c
Daniel Hendricks <mail@danhendricks.com>
2018-01-13 12:39:09 -0600

    Changed documentation link text

--- DESCRIPTION.md
+++ DESCRIPTION.md
@@ -1,5 +1,6 @@
 [![Latest Release](https://img.shields.io/github/release/cloudverve/plesk-terminal-icon-ext.svg?style=flat&label=current%20release)](https://github.com/cloudverve/plesk-terminal-icon-ext/releases)
+[![License](https://img.shields.io/github/license/cloudverve/plesk-terminal-icon-ext.svg)](https://raw.githubusercontent.com/cloudverve/plesk-terminal-icon-ext/master/LICENSE)
 
 This is a simple extension that adds an icon to the user's control panel for web-based SSH access. It is designed to be used in conjunction with [Shell in a Box](https://github.com/shellinabox/shellinabox).
 
-For instructions on installing Shell in a Box as a Docker container in Plesk, refer to the [wiki](https://github.com/cloudverve/plesk-terminal-icon-ext/wiki).
+For instructions on installing Shell in a Box as a Docker container in Plesk, refer to the [documentation](https://github.com/cloudverve/plesk-terminal-icon-ext/wiki).
--- plib/resources/locales/en-US.php
+++ plib/resources/locales/en-US.php
@@ -3,7 +3,7 @@ $messages = [
   'settingsPageTitle' => 'Terminal Icon',
   'clientPageTitle' => 'Terminal (SSH)',
   'clientIntroText' => 'Use your account username (%s) and password to connect via SSH.',
-  'settingsIntroText' => sprintf( 'Please provide the URL to your Shell in a Box instance. For more information, see the <a href="%s" target="_blank">wiki</a>.', 'https://github.com/cloudverve/plesk-terminal-icon-ext/wiki' ),
+  'settingsIntroText' => sprintf( 'Please provide the URL to your Shell in a Box instance. For more information, see the <a href="%s" target="_blank">documentation</a>.', 'https://github.com/cloudverve/plesk-terminal-icon-ext/wiki' ),
   'terminalUrlLabel' => 'Shell in a Box URL',
   'terminalUrlDescription' => sprintf( 'The URL to your Shell in a Box instance. Example: http://%s/siab', parse_url( $_SERVER['HTTP_HOST'], PHP_URL_HOST ) ),
   'terminalIconLocationLabel' => 'Move Icon to Sidebar',
