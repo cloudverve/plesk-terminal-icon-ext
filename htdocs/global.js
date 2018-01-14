8c95a318ee2c3971868c6812c31c9d4d5c823c60
Daniel Hendricks <mail@danhendricks.com>
2018-01-13 20:15:23 -0600

    :fire: Removed compiled assets from repo

--- htdocs/global.js
+++
@@ -1,9 +0,0 @@
-/**
- * A simple extension that adds an icon to the user's control panel for web-based SSH access via Shell in a Box.
- *
- * @version 1.0.0
- * @license Apache-2.0
- * @see {@link https://github.com/cloudverve/plesk-terminal-icon-ext}
- */
-
-Jsw.namespace( 'PleskExt.TerminalIcon' );
